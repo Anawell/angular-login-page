542fc3d5c28b5daed83e42edd7e7a67c09107d2f
Anaelle <thomas.anawel@gmail.com>
2020-11-28 08:20:37 +0100

    feat: add feedback success component

--- src/app/components/auth/signin-feedback/signin-feedback.component.ts
+++ src/app/components/auth/signin-feedback/signin-feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-signin-feedback',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninFeedbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title,
+              private meta: Meta) { }
 
   ngOnInit(): void {
   }
