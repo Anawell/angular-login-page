06dc9e79c959e73a756ea59a73d838750c5cce0f
Anaelle <thomas.anawel@gmail.com>
2020-11-28 08:01:18 +0100

    feat: prepare appComponent and metaTags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'login-page';
+
+export class AppComponent implements OnInit {
+
+  title = 'AngularApp | Webkomplet';
+
+  constructor(private titleSeo: Title,
+              private metaSeo: Meta) { }
+
+  ngOnInit(): void {
+    this.initSeoMeta();
+  }
+
+  initSeoMeta() {
+    this.metaSeo.addTags([
+      { name: 'robots', content: 'noindex, nofollow' }
+    ])
+    this.titleSeo.setTitle('AngularApp | Webkomplet');
+  }
+  
 }
